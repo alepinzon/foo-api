aa3499f7f6720412f3c7093764415ced5b76ba33
Alejandro <alejandro@osigu.com>
2018-03-27 21:21:15 +0000

    rename FooApiApplication to Application

--- src/main/java/io/foo/Application.java
+++ src/main/java/io/foo/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FooApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FooApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
